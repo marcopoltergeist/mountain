dfbc5c19cce10f682e0aa326690d13087755fab3
marcopoltergeist <paul_dubor@outlook.fr>
2024-03-30 11:49:31 +0100

    add component to homepage

---
+++ frontend/src/components/HomePage/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../../style/Nav.css";
+
+function Nav() {
+  return (
+    <section>
+      <ul>
+        <li>Mountain</li>
+        <li>FAQ</li>
+        <li>Contact</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Nav;
---
+++ frontend/src/components/HomePage/TitleHome.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../../style/TitleHome.css";
+
+function TitleHome() {
+  return <div>TitleHome</div>;
+}
+
+export default TitleHome;
