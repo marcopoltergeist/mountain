a0c411462ef3a5bbf8da9f6887be3af87b179f5a
marcopoltergeist <paul_dubor@outlook.fr>
2024-04-04 14:44:32 +0200

    new branch

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,4 @@
--- Table pour les lecteurs
+
 CREATE TABLE reader (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE reader (
   username VARCHAR(100) NOT NULL
 );
 
--- Table pour les articles de montagne
+
 CREATE TABLE mountain_article (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   title VARCHAR(100) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE mountain_article (
   content TEXT NOT NULL
 );
 
--- Table pour les lectures des articles de montagne par les lecteurs
+
 CREATE TABLE reading_mountain_article (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   reader_id INT NOT NULL,
@@ -23,4 +23,21 @@ CREATE TABLE reading_mountain_article (
   FOREIGN KEY (reader_id) REFERENCES reader(id),
   FOREIGN KEY (article_id) REFERENCES mountain_article(id)
 );
+
+
+INSERT INTO reader (email, password, username) VALUES
+('mallory@gmail.com', 'everest', 'malo'),
+('inox@gmail.com', 'everest', 'ines');
+
+
+INSERT INTO mountain_article (title, author, content) VALUES 
+('Les sommets majestueux des Alpes', 'Jean Dupont', 'Un article sur les sommets majestueux des Alpes.'),
+('Les défis de l'escalade en Himalaya', 'Alice Martin', 'Un article sur les défis de l'escalade en Himalaya.');
+
+
+INSERT INTO reading_mountain_article (reader_id, article_id, opinion) VALUES 
+(1, 1, "Un article intéressant sur les Alpes."),
+(1, 2, "Fascinant ! Je rêve de gravir les sommets de l'Himalaya."),
+(2, 1, "J'aime lire sur les montagnes, merci pour cet article.");
+
 
